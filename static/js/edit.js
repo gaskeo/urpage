fa91ee86b3edeb64f003bd4a74eebb85fd948081
Tikovka72 <zeytunyan2004@mail.ru>
2021-06-05 23:43:56 +0500

    some improves in css and js

--- static/js/edit.js
+++ static/js/edit.js
@@ -1,11 +1,20 @@
 function changeTab(evt, tab, section) {
-    let i, sections, tabs;
+    let i, sections, tabs, activeSection;
 
     sections = document.getElementsByClassName("section");
 
+    activeSection = document.getElementById(section)
+
     for (i = 0; i < sections.length; i++) {
-        sections[i].style.opacity = "0";
-        sections[i].style.display = "none";
+        if (sections[i] !== activeSection) {
+            sections[i].style.opacity = "0";
+            sections[i].style.display = "none";
+        }
+        else {
+            activeSection.style.opacity = "1"
+            activeSection.style.display = "block"
+        }
+
     }
 
     tabs = document.getElementsByClassName("tabs-button active-tab");
@@ -15,10 +24,6 @@ function changeTab(evt, tab, section) {
     }
 
     evt.currentTarget.className += " active-tab";
-    document.getElementById(section).style.display = "block";
-
-    document.getElementById(section).style.opacity = "1";
-
 }
 
 window.onload = function () {
@@ -27,7 +32,8 @@ window.onload = function () {
         input.type = 'file';
         input.accept = ".jpg, .jpeg, .png";
         input.onchange = e => {
-            let file = e.target.files[0];
+            let form, file
+            file = e.target.files[0];
             document.getElementById("img_src").src = window.URL.createObjectURL(file);
             form.image = input
         };
@@ -36,19 +42,19 @@ window.onload = function () {
 };
 
 function deleteLink(divId) {
-    let elem, index, elemsN, i
+    let elem, index, countElements, i
     elem = document.getElementById(divId)
     index = divId.replace("link-div-", "")
-    elemsN = document.getElementsByClassName("page-form-link").length
+    countElements = document.getElementsByClassName("page-form-link").length
 
-    if (parseInt(index) !== elemsN - 1) {
-        for (i = parseInt(index) + 1; i < elemsN; i++) {
-            document.getElementById("link-div-" + i).id = "link-div-" + (i - 1)
-            document.getElementById("link-" + i).id = "link-" + (i - 1)
+    if (parseInt(index) !== countElements - 1) {
+        for (i = parseInt(index) + 1; i < countElements; i++) {
+            document.getElementById(`link-div-${i}`).id = `link-div-${(i - 1)}`
+            document.getElementById(`link-${i}`).id = `link-${(i - 1)}`
 
-            document.getElementById("delete-link-" + i).setAttribute("onclick", "deleteLink('link-div-" + (i - 1) + "')")
+            document.getElementById(`delete-link-${i}`).setAttribute("onclick", `deleteLink('link-div-${(i - 1)}')`)
 
-            document.getElementById("delete-link-" + i).id = "delete-link-" + (i - 1)
+            document.getElementById(`delete-link-${i}`).id = `delete-link-${(i - 1)}`
         }
     }
     elem.remove()
@@ -56,27 +62,27 @@ function deleteLink(divId) {
 
 
 function addLink() {
-    let elemsN, form, newDiv, newInput, newButton, afterItem, newP
-    elemsN = document.getElementsByClassName("page-form-link").length
+    let newElementN, form, newDiv, newInput, newButton, afterItem
+    newElementN = document.getElementsByClassName("page-form-link").length
 
     form = document.getElementById("page-form-links")
     afterItem  = document.getElementById("add-link-button")
 
     newDiv = document.createElement("div")
-    newDiv.id = "link-div-" + elemsN
+    newDiv.id = `link-div-${newElementN}`
     newDiv.style.display = "inline"
     newDiv.style.verticalAlign = "middle"
 
     newInput = document.createElement("input")
-    newInput.id = "link-" + elemsN
+    newInput.id = `link-${newElementN}`
     newInput.name = "link"
     newInput.className = "page-form-input page-form-link"
     newInput.type = "text"
 
     newButton = document.createElement("button")
-    newButton.id = "delete-link-" + elemsN
+    newButton.id = `delete-link-${newElementN}`
     newButton.className = "delete-link-button"
-    newButton.setAttribute("onclick", "deleteLink('link-div-" + elemsN + "')")
+    newButton.setAttribute("onclick", `deleteLink('link-div-${newElementN}')`)
     newButton.textContent = "-"
     newButton.type = "button"
 
